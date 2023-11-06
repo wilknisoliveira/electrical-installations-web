def2bad23ba1341ff47de57c859e53caad4896d4
Wilknis Deyvis <wilknisoliveira@gmail.com>
2023-11-06 13:44:40 -0300

    restarting the project

--- README.md
+++
@@ -1,2 +0,0 @@
-# InstalacoesEletricasWeb
-Este projeto tem como objetivo o desenvolvimento de um software para dimensionamento de instalações elétricas.
--- public/js/index.js
+++
@@ -1,6 +0,0 @@
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        const pageHeader = document.querySelector('div#pageHeader')
-        pageHeader.innerHTML = data;
-    }) 
