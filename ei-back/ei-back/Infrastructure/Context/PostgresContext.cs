0533e9417b513fee043f006553115bfc1f6498d2
Wilknis Deyvis <wilknisoliveira@gmail.com>
2023-11-30 08:39:17 -0300

    config: postgres support

---
+++ ei-back/ei-back/Infrastructure/Context/PostgresContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ei_back.Infrastructure.Context
+{
+    public class PostgresContext : DbContext
+    {
+        public PostgresContext() { }
+
+        public PostgresContext(DbContextOptions<PostgresContext> options) : base(options) { }
+    }
+}
--- ei-back/ei-back/Program.cs
+++ ei-back/ei-back/Program.cs
@@ -1,3 +1,6 @@
+using ei_back.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var connection = builder.Configuration["PostgresConnection:PostgresConnectionString"];
+builder.Services.AddDbContext<PostgresContext>(options => options.UseNpgsql(connection));
+
 
 //Apply the Dependecy Injection here!
 
