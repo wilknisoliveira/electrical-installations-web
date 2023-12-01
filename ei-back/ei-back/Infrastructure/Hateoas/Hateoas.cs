435ba837af8d32b7cc1ea08dfcf4cf0c385c253d
Wilknis Deyvis <wilknisoliveira@gmail.com>
2023-12-01 08:35:44 -0300

    config: HATEOAS support

---
+++ ei-back/ei-back/Infrastructure/Hateoas/Hateoas.cs
@@ -0,0 +1,40 @@
+﻿namespace ei_back.Infrastructure.Hateoas
+{
+    public class Hateoas
+    {
+        private string url;
+        private string protocol = "https://";
+        public List<Link> actions = new List<Link>();
+
+        public Hateoas(string url)
+        {
+            this.url = url;
+        }
+
+        public Hateoas(string url, string protocol)
+        {
+            this.url = url;
+            this.protocol = protocol;
+        }
+
+        public void AddAction(string rel, string method)
+        {
+            actions.Add(new Link(this.protocol + this.url, rel, method));
+        }
+
+        public Link[] GetActions(string sufix)
+        {
+            Link[] tempLinks = new Link[actions.Count];
+            for (int i = 0; i < tempLinks.Length; i++)
+            {
+                tempLinks[i] = new Link(actions[i].href, actions[i].rel, actions[i].method);
+            }
+
+            foreach (var link in tempLinks)
+            {
+                link.href = link.href + "/" + sufix;
+            }
+            return tempLinks;
+        }
+    }
+}
---
+++ ei-back/ei-back/Infrastructure/Hateoas/Link.cs
@@ -0,0 +1,16 @@
+﻿namespace ei_back.Infrastructure.Hateoas
+{
+    public class Link
+    {
+        public string href {  get; set; }
+        public string rel { get; set; }
+        public string method { get; set; }
+
+        public Link(string href, string rel, string method)
+        {
+            this.href = href;
+            this.rel = rel;
+            this.method = method;
+        }
+    }
+}
