4eeb2db7a5e9d669f6aa0484f9357ba46e1595a7
Wilknis Deyvis <wilknisoliveira@gmail.com>
2023-12-17 10:28:53 -0300

    feat: some comments

--- ei-back/ei-back/Domain/Base/BaseEntity.cs
+++ ei-back/ei-back/Domain/Base/BaseEntity.cs
@@ -5,14 +5,10 @@ namespace ei_back.Domain.Base
 {
     public class BaseEntity
     {
-        //[Key]
-        //[Column("id")]
         public Guid Id { get; set; }
 
-        //[Column("created_at", TypeName = "timestamp without time zone")]
         public DateTime CreatedAt { get; set; }
 
-        //[Column("updated_at", TypeName = "timestamp without time zone")]
         public DateTime UpdatedAt { get; set; }
 
     }
